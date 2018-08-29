42797e526661e710ee9b9661193c1ed581e8ffc2
newblock <39322267+newblock@users.noreply.github.com>
2018-08-29 12:12:03 +0800

    Update login.js
    type='password'

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -88,6 +88,7 @@ class Login extends React.Component
             <WhiteSpace></WhiteSpace>
             <InputItem
               onChange={v=>this.handleChange('pwd',v)}
+              type='password'
               >密码</InputItem>
           </List>
           <WhiteSpace></WhiteSpace>
